5cfd77a08f55636e3eb0dc4171a6fd90f4f0655f
Tunnox <chukwuemekaumunna@gmail.com>
2025-06-02 00:48:18 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 import psycopg2
-import os
 
 app = Flask(__name__)
 app.secret_key = 'Chuk_secret_key'  # Replace with your secret key
